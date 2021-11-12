e8b54482a689c22cbc3802058af4ff697137a7b0
Irfan Sheikh <irfan.sh3112@gmail.com>
2021-11-12 23:57:57 +0530

    added code to fetch images using fetch API

--- frontend/art-point/src/components/ArtItem.js
+++ frontend/art-point/src/components/ArtItem.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+
 const ArtItem = (props) => {
-    const imageUrl = "http://localhost:8080/images/" + props.art.id + ".png";
+    const [imgObjURL, setImgObjURL] = useState('');
+
+    const imageURL = "http://localhost:8080/images/" + props.art.id + ".png";
+
+    useEffect(() => {
+        fetch(imageURL)
+        .then(res => res.blob())
+        .then(imgBlob => {
+            const url = URL.createObjectURL(imgBlob);
+            setImgObjURL(url);
+            console.log(url);
+        })
+    }, [imageURL]);
+    
     return <div>
         {props.art.title}
-        <img src={imageUrl} alt="bye" />
+        <img src={imgObjURL} alt={props.art.title} />
     </div>;
 }
 
