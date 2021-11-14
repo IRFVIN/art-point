fbb41146966688ff6b1859813b63dd2ec8581d6b
Irfan Sheikh <irfan.sh3112@gmail.com>
2021-11-14 18:55:57 +0530

    created 2 column grid layout in ArtDetailView

--- frontend/art-point/src/components/art/views/ArtDetailView.js
+++ frontend/art-point/src/components/art/views/ArtDetailView.js
@@ -1,6 +1,9 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button, CardMedia, Grid, Rating, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const ArtDetailView = () => {
     const [imgObjURL, setImgObjURL] = useState('');
@@ -17,6 +20,8 @@ const ArtDetailView = () => {
         );
     }, [url]);
 
+    art.rating = 3.5;
+
     const imageURL = "http://localhost:8080/images/" + params.artId + ".png";
 
     useEffect(() => {
@@ -29,33 +34,73 @@ const ArtDetailView = () => {
             })
     }, [imageURL]);
 
+
     return (
-        <Card sx={{
-            maxWidth: 400,
-            margin: "auto"
-        }}>
-            <CardMedia
-                component="img"
-                // height="140"
-                // image={imageURL}
-                image={imgObjURL}
-                alt={art.title}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {art.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {art.description}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Buy Now</Button>
-                <Button size="small">Add to Cart</Button>
-            </CardActions>
-        </Card>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={3}>
+                <Grid item md>
+                    <CardMedia
+                        component="img"
+                        image={imgObjURL}
+                        alt={art.title}
+                    />
+                </Grid>
+                <Grid item md>
+                    <Typography variant="h4" color="primary">
+                            {art.title}
+                    </Typography>
+                    <Typography color="textSecondary">
+                            by {art.owner ? art.owner : "null"}
+                    </Typography>
+                    
+                    <Rating name="read-only" value={art.rating} precision={0.5} readOnly />
+
+                    <Typography variant="h6" color="textSecondary">
+                        Description
+                    </Typography>
+                    <Typography color="textPrimary">
+                        {art.description}
+                    </Typography>
+
+                    <Typography style={{marginTop:10}} variant="h6" color="textSecondary">
+                        Price: &#8377;{art.price}
+                    </Typography>
+
+                    <Button style={{marginTop:10}}
+                        //component={Link} to={'#'}
+                        size="small" variant="contained"
+                        startIcon={<AddShoppingCartIcon/>}
+                        onClick={() => {console.log("add to cart: " + art.id);}}
+                    >
+                        Add to Cart
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
+
     );
 }
 
 export default ArtDetailView;
 
+
+/*
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function AutoGrid() {
+  return (
+
+  );
+}
+*/
