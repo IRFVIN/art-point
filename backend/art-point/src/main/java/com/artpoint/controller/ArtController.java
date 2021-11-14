4f3034f70896ccf153c3eeec473ed75879c31988
Irfan Sheikh <irfan.sh3112@gmail.com>
2021-11-15 00:28:20 +0530

    handle multipart form data in addArt

--- backend/art-point/src/main/java/com/artpoint/controller/ArtController.java
+++ backend/art-point/src/main/java/com/artpoint/controller/ArtController.java
@@ -7,13 +7,14 @@ import com.artpoint.entity.Art;
 import com.artpoint.service.ArtService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,21 +41,26 @@ public class ArtController {
     @ApiResponses(value = {
         @ApiResponse(
             responseCode = "200",
-            description = "Successfully added the art"
+            description = "Successfully added the art",
+            content = {@Content(mediaType = "multipart/form-data")}
         )
     })
-    @PostMapping("/art")
-    public String addArt(/*@RequestBody Art art, */@RequestParam("file") MultipartFile file) throws Exception {
-//        artService.addArt(art);
-
-        return saveFile(file, 150L);
+    @PostMapping(value = "/art", consumes = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.MULTIPART_FORM_DATA_VALUE
+    })
+    public void addArt(@RequestPart("art") Art art, @RequestPart("image") MultipartFile imageFile) throws Exception {
+        artService.addArt(art);
+        System.out.println(imageFile);
+        //System.out.println(new ClassPathResource("static/image/151.jpeg"));
+        //System.out.println(saveImage(imageFile, 150L));
     }
 
-    public String saveFile(MultipartFile file, Long artId) throws Exception {
+    public String saveImage(MultipartFile imageFile, Long artId) throws Exception {
         String dirPath = "/home/vineetkumar19/Desktop/ecom/Art Point/art-point/backend/art-point/src/main/resources/static/image/";
 
-        String fileName = artId.toString() + '.' + getFileExtension(file.getOriginalFilename());
-        file.transferTo(new File(dirPath + fileName));
+        String fileName = artId.toString() + '.' + getFileExtension(imageFile.getOriginalFilename());
+        imageFile.transferTo(new File(dirPath + fileName));
         return "file updated successfully";
 //        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(fileName).toUriString();
     }
