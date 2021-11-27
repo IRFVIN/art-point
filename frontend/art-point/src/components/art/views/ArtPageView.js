6e10c2fd4b6ae686ba95a7aefd3bd2f00c3a1bf1
Irfan Sheikh <irfan.sh3112@gmail.com>
2021-11-27 17:34:02 +0530

    add search by title form

--- frontend/art-point/src/components/art/views/ArtPageView.js
+++ frontend/art-point/src/components/art/views/ArtPageView.js
@@ -1,6 +1,8 @@
 import ArtGridView from "./ArtGridView";
 import { useEffect, useState } from "react";
-import { Container, Pagination } from "@mui/material";
+import { Button, Container, IconButton, Input, InputAdornment, Pagination, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import SearchIcon from '@mui/icons-material/Search';
 
 const ArtPageView = () => {
     const [arts, setArts] = useState([]);
@@ -9,30 +11,46 @@ const ArtPageView = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
 
-    const handleChange = (e, p) => {
+    const handlePageChange = (e, p) => {
         console.log(p);
         setCurrentPage(p);
+    }
+
+    const handleSearchTitleChange = (e) => {
+        const newSearchTitle = e.target.value;
+        console.log(newSearchTitle);
+        setSearchTitle(newSearchTitle);
+    }
+
+    const handleSubmit = (e) => {
+        console.log(e);
     }
 
     console.log("heeeyeyey");
 
     useEffect(() => {
-        const url = `http://localhost:8080/art?page=${currentPage - 1}`;
+        let url = `http://localhost:8080/art?page=${currentPage - 1}`;
+        if (searchTitle != "") url += `&title=${searchTitle}`;
+        
         fetch(url).then(res => {
             return res.json();
         }).then(res => {
             setArts(res.art);
             setTotalPages(res.totalPages);
         });
-    }, [currentPage]);
+    }, [searchTitle, currentPage]);
 
     return (
         <Container>
+            <form onSubmit={handleSubmit}>
+                <TextField variant="outlined" placeholder="Search by Title" onChange={handleSearchTitleChange}/>
+            </form>
+
             <ArtGridView arts={arts} />
             <Pagination
                 count={totalPages}
                 size="large"
-                onChange={handleChange}
+                onChange={handlePageChange}
             />
         </Container>
     );
